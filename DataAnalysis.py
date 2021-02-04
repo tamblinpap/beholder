d7bfc53af4b480463f8ddc319848d02553dad05f
RatKid <tamblinpap@gmail.com>
2021-02-04 16:13:19 -0500

    DataAnalysis.py can create csv files with SMA info.  Created AlgoTester.py to run simluations on SMA datasheets

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -31,12 +31,12 @@ except:
 
 # converts the date strings in the index into pandas datetime format
 closePrice.index = pd.to_datetime(closePrice.index)
-closePrice.plot()
-#plt.show()
+# closePrice.plot()
+# plt.show()
 
-smaMAX = closePrice.rolling(window=10).mean()
+sma20 = closePrice.rolling(window=20).mean()
 sma50 = closePrice.rolling(window=50).mean()
-print(smaMAX)
+sma150 = closePrice.rolling(window=200).mean()
 
 #chart style
 plt.style.use('dark_background')
@@ -45,8 +45,9 @@ plt.figure(figsize=(12, 6))
 
 #plotting price and SMA line in plt
 plt.plot(closePrice, label='Adj Close', linewidth=2)
-plt.plot(sma50, label='50 day rolling SMA', linewidth=1)
-plt.plot(smaMAX, label='10 dat rolling SMA', linewidth=2)
+plt.plot(sma150, label='150 day rolling SMA', linewidth=3)
+plt.plot(sma50, label='50 day rolling SMA', linewidth=2)
+plt.plot(sma20, label='20 dat rolling SMA', linewidth=1)
 
 #adds title and labels on the axes, making legend visible
 plt.xlabel('Date')
@@ -55,3 +56,13 @@ plt.title('Price with Simple Moving Average for 50 and 10 days')
 plt.legend()
 
 plt.show()
+
+# this processes the data into a data frame I can use
+SMAPrice_df = pd.DataFrame({
+    'Adj Close': closePrice,
+    'SMA 20': sma20,
+    'SMA 50': sma50,
+    'SMA 150': sma150
+})
+
+SMAPrice_df.to_csv(dataFile + '_SMAs.csv')
