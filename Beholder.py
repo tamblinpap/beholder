94671c2ea1402c987535d525189d426903fd61dd
RatKid <tamblinpap@gmail.com>
2021-02-07 22:46:14 -0500

    Added handling in Beholder.py for switching modes, text input, and started work on test mode

--- Beholder.py
+++ Beholder.py
@@ -11,20 +11,41 @@ import math
 from datetime import datetime
 from pycoingecko import CoinGeckoAPI
 
+
 userInput = ''
+currentMode = ''
 
 
 def SaveAsCSV(dataFrame):
     dataFrame.to_csv('Data/' + dataFrame + '.csv')
 
 
-def ParseUserInput(input):
-    if input == 'exit':
+def ParseUserInput(inputStr):
+    if inputStr == 'exit':
         return
+    elif inputStr[0:2] == '-m':
+        modeInput = inputStr[3:len(inputStr)]
+        if modeInput == 'Test' or modeInput == 'test':
+            ModeTest()
+        elif modeInput == 'PaperTrade':
+            ModePaperTrade()
+        elif modeInput == 'ActualTrade':
+            ModeActualTrade()
+        else:
+            print(modeInput + ' is not a valid mode, check the readme for help.')
+    elif currentMode == 'test':
+        if inputStr == 'menu':
+            return
+        elif inputStr.isupper():
+            GetData(inputStr)
+        else:
+            print('Not a valid command. Refer to readme on how to use test mode. type return to go back to menu')
+    else:
+        print('Not a valid command.')
 
 
 def GetData(tickerCode):
-    print('Getting historical data for ' + tickerCode + '...')
+    print('Attempting to get historical data for ' + tickerCode + '...')
 
 
 def AlgoTester(dataFrame, algo, stockName):
@@ -32,7 +53,20 @@ def AlgoTester(dataFrame, algo, stockName):
 
 
 def ModeTest():
-    print('Launching in test mode...')
+    global currentMode
+    global userInput
+    if currentMode != 'test':
+        print('Launching in test mode...\n')
+        print('Test mode is used mainly for testing the effectiveness of Beholder and its eyes on past data.')
+        print('Test mode will run a simulation on all historical data available of a certain stock or crypto.')
+        print('Enter ticker info for holding you would like to track.\n')
+    currentMode = 'test'
+    while userInput != 'return' and userInput != 'exit':
+        print('BeholderCMD/Test: ', end='')
+        userInput = input()
+        ParseUserInput(userInput)
+    print('Exiting test mode...')
+    currentMode = 'main'
 
 
 def ModePaperTrade():
@@ -44,15 +78,19 @@ def ModeActualTrade():
 
 
 # Starting user interaction
-beholderText = open('Data\Beholder.txt')
-for element in beholderText:
-    print(element)
-print('Welcome to the "Beholder" bot!\n')
-print('Please refer to the README.md file for operation instructions')
-print('\nThe creator of this bot is not liable for any losses or data theft that may happen as a result of this bot')
-print('The creator of this bot is not a financial advisor.  Use at your own risk')
-print('Beholder Bot was created by Tamblin Papendorp')
-print('\nWhat mode to you want to launch Beholder in?')
+if currentMode == '':
+    beholderText = open('.\Data\Beholder.txt')
+    for element in beholderText:
+        print(element)
+    print('Welcome to the "Beholder" bot!\n')
+    print('Please refer to the README.md file for operation instructions')
+    print(
+        '\nThe creator of this bot is not liable for any losses or data theft that may happen as a result of this bot')
+    print('The creator of this bot is not a financial advisor.  Use at your own risk')
+    print('Beholder Bot was created by Tamblin Papendorp')
+    print('\nWhat mode to you want to launch Beholder in?')
+
+currentMode = 'menu'
 
 while userInput != 'exit':
     print('BeholderCMD: ', end='')
